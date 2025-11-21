7d5c39814575a24867926a024b7d8db88ed4094c
Todd Strader <tstrader@hudson-trading.com>
2025-11-21 12:38:36 -0500

    Add GitHub workflow

---
+++ ci/test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Copyright (c) 1991-2004 Pragmatic C Software Corp.
+
+set -e
+set -x
+
+! ./vcddiff examples/counter.vcd examples/counter.vcd | grep -q .
+! ./vcddiff examples/counter.vcd examples/counter.time.no_diff.vcd | grep -q .
+! ./vcddiff examples/counter.vcd examples/counter.change_reorder.no_diff.vcd | grep -q .
+! ./vcddiff examples/counter.vcd examples/counter.var_reorder.no_diff.vcd | grep -q .
+! ./vcddiff examples/counter.vcd examples/counter.identifier.no_diff.vcd | grep -q .
+
+./vcddiff examples/counter.vcd examples/counter.end_time.diff.vcd |
+    grep "Files have different end times"
+./vcddiff examples/counter.vcd examples/counter.edge_time.diff.vcd |
+    grep "t.clk .* at time 20 next occurence at time 21"
+./vcddiff examples/counter.vcd examples/counter.sig_name.diff.vcd |
+    grep "not defined in both files"
+./vcddiff examples/counter.vcd examples/counter.new_sig.diff.vcd |
+    grep "Ignoring signal t.the_sub.new_sig .* - not defined in both files"
