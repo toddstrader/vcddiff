5c6a7e6cb43bb2727a593718073f26396f12afe3
Todd Strader <tstrader@hudson-trading.com>
2025-11-21 08:22:46 -0500

    Allow FIFO inputs

--- ci/test.sh
+++ ci/test.sh
@@ -4,11 +4,20 @@
 set -e
 set -x
 
+if [[ "$(uname -s)" != MINGW* && "$(uname -s)" != MSYS* && "$(uname -s)" != CYGWIN* ]]; then
+  FIFO_SUPPORTED=1
+else
+  FIFO_SUPPORTED=0
+fi
+
 ! ./vcddiff examples/counter.vcd examples/counter.vcd | grep -q .
 ! ./vcddiff examples/counter.vcd examples/counter.time.no_diff.vcd | grep -q .
 ! ./vcddiff examples/counter.vcd examples/counter.change_reorder.no_diff.vcd | grep -q .
 ! ./vcddiff examples/counter.vcd examples/counter.var_reorder.no_diff.vcd | grep -q .
 ! ./vcddiff examples/counter.vcd examples/counter.identifier.no_diff.vcd | grep -q .
+if [ "$FIFO_SUPPORTED" = "1" ]; then
+  ! ./vcddiff <(cat examples/counter.vcd) <(cat examples/counter.vcd) | grep -q .
+fi
 
 ./vcddiff examples/counter.vcd examples/counter.end_time.diff.vcd |
     grep "Files have different end times"
@@ -18,3 +27,7 @@ set -x
     grep "not defined in both files"
 ./vcddiff examples/counter.vcd examples/counter.new_sig.diff.vcd |
     grep "Ignoring signal t.the_sub.new_sig .* - not defined in both files"
+if [ "$FIFO_SUPPORTED" = "1" ]; then
+  ./vcddiff <(cat examples/counter.vcd) <(cat examples/counter.end_time.diff.vcd) |
+      grep "Files have different end times"
+fi
